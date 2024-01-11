b5295fb866ba1ba7bb566921d9cd5f60f119f483
= <arparvez50@gmail.com>
2024-01-11 12:53:59 +0600

    Hero slider with slick

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('Hello world!');
+let mobileMenuIcon = document.querySelector(".menu__icon a");
+let menu = document.querySelector(".links");
+
+mobileMenuIcon.addEventListener("click", function (e) {
+    e.preventDefault();
+    menu.classList.toggle("active");
+    console.log("clicked");
+});
+
+// click slider for hero section
+$(function () {
+    // jQuery methods go here...
+    $(".sliders").slick();
+});
